7a8dee93900985ebb5cf03be89ebb956bb59b386
Benji <benjamin@lemoine.cloud>
2026-09-22 18:09:45 +0200

    Branche GamePage sur /games/:id et retire la route de test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { ProtectedRoute } from "./components/ProtectedRoute";
-import { TestPlateau } from "./dev/TestPlateau";
 import { LoginPage } from "./pages/LoginPage";
 import { SignupPage } from "./pages/SignupPage";
 import { CreateGame } from "./pages/CreateGame";
+import { GamePage } from "./pages/GamePage";
 import "./App.css";
 
 function Placeholder({ label }: { label: string }) {
@@ -20,14 +20,11 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
 
-        {/* Route de dev temporaire, a retirer une fois GamePage pret (voir src/dev/) */}
-        <Route path="/test-echecs" element={<TestPlateau />} />
-
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Placeholder label="Parties en cours" />} />
           <Route path="/create-game" element={<CreateGame />} />
           <Route path="/history" element={<Placeholder label="Historique" />} />
-          <Route path="/games/:id" element={<Placeholder label="Partie" />} />
+          <Route path="/games/:id" element={<GamePage />} />
         </Route>
       </Routes>
     </>
